c408c352f423c567df75b649ad144405fdc4a8ee
osasou <34133484+osasou@users.noreply.github.com>
2020-10-25 10:05:25 +0900

    create complete button func

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,14 @@ export const App = () => {
 
   const onClickDelete = (index) => {
     const newTodos = [...incompleteTodos];
-    newTodos.splice(index,1);
+    newTodos.splice(index, 1);
     setIncompleteTodos(newTodos);
+  };
+
+  const onClickComplete = (index) => {
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setCompleteTodos(newCompleteTodos);
+    onClickDelete(index);
   };
 
   return (
@@ -41,7 +47,7 @@ export const App = () => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>完了</button>
+                <button onClick={() => onClickComplete(index)}>完了</button>
                 <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
